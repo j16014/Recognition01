306488214beae79e7da72dc5096be6c1c9c72b20
j16014 <j16014@sangi.jp>
2018-05-17 12:06:36 +0900

    First Commit

---
+++ src/recognition01/Recognition01_main.java
@@ -0,0 +1,29 @@
+package recognition01;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.DetectFacesOptions;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.DetectedFaces;
+
+public class Recognition01_main {
+
+	public static	void main(String[] orgs) {
+		VisualRecognition service = new VisualRecognition("2018-03-19");
+		service.setApiKey("4f771d96592f197797ac81dc10e3ab1e678d0ebf");
+
+		DetectFacesOptions detectFacesOptions = null;
+		try {
+			detectFacesOptions = new DetectFacesOptions.Builder()
+			  .imagesFile(new File("img/prez.jpg"))
+			  .build();
+		} catch (FileNotFoundException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		DetectedFaces result = service.detectFaces(detectFacesOptions).execute();
+		System.out.println(result);
+	}
+
+}
